f95ce09773f8b20dced059a8ddb9d9264336c014
Andrew Lounsbury <lounsburya@gmail.com>
2022-12-08 17:50:46 -0600

    1-5 complete

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -1,4 +1,14 @@
-let firstCard = 6
-let secondCard = 9
+let firstCard = 11
+let secondCard = 12
 let sum = firstCard + secondCard
 
+if (sum < 21) {
+    console.log('Do you want to draw a new car?');
+    
+}else if (sum === 21){
+    console.log('Congrats! Black Jack!');
+    
+}else {
+    console.log('over 21, game over');
+    
+}
